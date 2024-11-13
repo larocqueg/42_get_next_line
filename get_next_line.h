4cc82d98e9d7b1bac477dd06499f620a27ab0753
larocqueg <portugalgabriel2021@gmail.com>
2024-11-13 11:29:03 +0000

    updated .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/13 11:23:41 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/11/13 11:24:56 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/11/13 11:28:51 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+
+char	*get_next_line(int fd);
 
 #endif
