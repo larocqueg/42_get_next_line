ed60640728e08f518294b4eb7f5329f008f1bcab
larocqueg <portugalgabriel2021@gmail.com>
2024-11-14 15:51:25 +0000

    ft_read ok

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,58 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:40:39 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/11/14 14:40:48 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/11/14 15:50:48 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+static char	*ft_read(int fd, char *buffer);
+static char	*ft_get_line(char *buffer);
+static char	*ft_update_line(char *buffer);
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = ft_read(fd, buffer);
+	if (!buffer)
+		return (NULL);
+	line = ft_get_line(buffer);
+	buffer = ft_update_line(buffer);
+	return (line);
+}
+
+static char	*ft_read(int fd, char *file)
+{
+	char	*buffer;
+	ssize_t	bytes;
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes = 1;
+	while (!ft_strchar(file, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes == -1)
+			return (free(buffer), NULL);
+		buffer[bytes] = '\0';
+		ft_strjoing(buffer, file);
+		if (!file)
+			return (free(buffer), free(file), NULL);
+		free(buffer);
+		return (file);
+	}
+}
+
+static char	*ft_get_line(char *buffer)
+{
+}
+
+static char	*ft_update_line(char *buffer)
+{
+}
