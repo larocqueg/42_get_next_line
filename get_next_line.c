44295ceaea9ed4f99bd4a3c711c57a43bb6ccdcb
larocqueg <portugalgabriel2021@gmail.com>
2024-11-14 15:17:17 +0000

    1

--- get_next_line.c
+++ get_next_line.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/13 11:23:41 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/11/14 14:57:01 by gde-la-r         ###   ########.fr       */
+/*   Created: 2024/11/14 14:40:39 by gde-la-r          #+#    #+#             */
+/*   Updated: 2024/11/14 14:40:48 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
-# endif
-
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-
-char	*get_next_line(int fd);
-
-#endif
+#include "get_next_line.h"
