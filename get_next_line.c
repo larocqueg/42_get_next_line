63a0068be03fff5ef5e065deb0000f9dffe404a3
larocqueg <portugalgabriel2021@gmail.com>
2024-11-14 16:16:16 +0000

    redoing function

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 14:40:39 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/11/14 16:03:32 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2024/11/14 16:15:38 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,27 @@ char	*get_next_line(int fd)
 
 static char	*ft_read(int fd, char *file)
 {
+	int		i;
+	char	*buffer;
+	ssize_t	r_content;
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	i = 0;
+	r_content = 1;
+	while (file[i] != '\n' && r_content > 0)
+	{
+		r_content = read(fd, file, BUFFER_SIZE);
+		if (r_content  = -1)
+			return (free(buffer), NULL);
+		buffer[r_content] = '\0';
+		file = ft_strjoin(file, buffer);
+		if (!file)
+			return (free(file), free(buffer), NULL);
+	}
+	free(buffer);
+	return (file);
 }
 
 static char	*ft_get_line(char *buffer)
