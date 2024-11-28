234f1dcdaec70f4c90b10705fc1ed89449fcefe3
Gabriel La Rocque <127416900+larocqueg@users.noreply.github.com>
2024-11-28 17:01:45 +0000

    Update main.c

--- main.c
+++ main.c
@@ -19,27 +19,24 @@ int main(int ac, char **av)
 {
     int fd;
     char *line;
-	(void)ac;
-    //char file[256];
-
-    //printf("File name: ");
-    //scanf("%255s", file);
+    (void)ac;
+	
     fd = open(av[1], O_RDONLY);
     if ((line = get_next_line(fd)) == NULL)
 	{
 		printf("%s\n", line);
 		return (-1);
 	}
-	else
-		printf("%s", line);
-	free(line);
-	line = NULL;
+    else
+	printf("%s", line);
+    free(line);
+    line = NULL;
     while ((line = get_next_line(fd)) != NULL)
     {
         printf("%s", line);
         free(line);
     }
     close(fd);
-	printf("\n");
+    printf("\n");
     return (0);
 }
