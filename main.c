236ea6b828130d3b29fdf535e0211383a71b2f89
larocqueg <portugalgabriel2021@gmail.com>
2024-11-28 16:52:06 +0000

    new main

--- main.c
+++ main.c
@@ -13,11 +13,15 @@ int main(int ac, char **av)
     //printf("File name: ");
     //scanf("%255s", file);
     fd = open(av[1], O_RDONLY);
-    if (fd == -1)
-    {
-        perror("Failed to open the file!");
-        return (1);
-    }
+    if ((line = get_next_line(fd)) == NULL)
+	{
+		printf("%s\n", line);
+		return (-1);
+	}
+	else
+		printf("%s", line);
+	free(line);
+	line = NULL;
     while ((line = get_next_line(fd)) != NULL)
     {
         printf("%s", line);
