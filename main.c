95bb3698fc5858fbdc9b2e8275f6ce3fea287f6d
larocqueg <portugalgabriel2021@gmail.com>
2025-01-25 15:57:24 +0000

    norm

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gde-la-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 16:56:36 by gde-la-r          #+#    #+#             */
-/*   Updated: 2024/11/28 16:56:38 by gde-la-r         ###   ########.fr       */
+/*   Updated: 2025/01/25 15:54:57 by gde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,27 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-    int fd;
-    char *line;
-    (void)ac;
-	
-    fd = open(av[1], O_RDONLY);
-    if ((line = get_next_line(fd)) == NULL)
-    {
-		printf("%s\n", line);
-		return (-1);
-    }
-    else
-	printf("%s", line);
-    free(line);
-    line = NULL;
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
-    close(fd);
-    printf("\n");
-    return (0);
+	int		fd;
+	char	*line;
+
+	(void)ac;
+	fd = open(av[1], O_RDONLY);
+	line = get_next_line(fd);
+	if (line == NULL)
+		return (printf("%s\n", line));
+	else
+		printf("%s", line);
+	free(line);
+	line = get_next_line(fd);
+	while (line)
+	{
+		printf("%s", line);
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	printf("\n");
+	return (0);
 }
