746f0c9534fda6362902d0f6f0a7bbf753c5a281
larocqueg <portugalgabriel2021@gmail.com>
2026-03-28 17:59:06 +0000

    Testing norminette in vscode

--- bonus/get_next_line_bonus.h
+++ bonus/get_next_line_bonus.h
@@ -6,27 +6,26 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/28 17:46:30 by gabriel           #+#    #+#             */
-/*   Updated: 2026/03/28 17:46:44 by gabriel          ###   ########.fr       */
+/*   Updated: 2026/03/28 17:57:49 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+// buffer_size definition
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
 # endif
 
 typedef struct s_gnl
 {
-	int fd;
-	char *curr;
-	struct s_gnl *next;
-} t_gnl;
-
-
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
-
+	int				fd;
+	char			*curr;
+	struct s_gnl	*next;
+}	t_gnl;
 #endif
